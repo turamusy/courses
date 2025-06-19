d7a71b0231210edcceb9558b39a795d4ea8a099c
turamusy <iwilltryhardboiz@gmail.com>
2025-06-19 22:17:10 +0300

    add nav routes

--- src/navigation/app-navigation.tsx
+++ src/navigation/app-navigation.tsx
@@ -4,6 +4,7 @@ import { RootStackParamList } from '../types/root-stack-param';
 import HomeScreen from '../screens/home/home-screen';
 import TopicSelectorScreen from '../screens/topic-selector/topic-selector-screen';
 import { createStackNavigator } from '@react-navigation/stack';
+import { NAVIGATION } from '../types/navigation-routes';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -11,8 +12,8 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="TopicSelector" component={TopicSelectorScreen} />
+        <Stack.Screen name={NAVIGATION.HOME}component={HomeScreen} />
+        <Stack.Screen name={NAVIGATION.TOPIC_SELECTOR} component={TopicSelectorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/types/navigation-routes.ts
@@ -0,0 +1,7 @@
+/**
+ * @description {object} NAVIGATION - An object containing screen names that will be imported every-time we use navigation.navigate().
+ */
+export enum NAVIGATION {
+  HOME = 'Home',
+  TOPIC_SELECTOR = 'TopicSelector',
+}
